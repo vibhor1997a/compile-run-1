4be808bc3d9c6a1b33f0fcce53713db99d308199
vibhor1997a <vibhor349a@rediffmail.com>
2017-10-16 11:15:00 +0530

    auto creating dir on startup

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ var cpp_support = require('./lib/cpp-support.js');
 var java_support=require('./lib/java-support.js');
 var python_support=require('./lib/python-support.js');
 var node_support=require('./lib/node-support.js');
+var path=require('path');
+
+if (!fs.existsSync('./code')) {
+    fs.mkdirSync('./code', 0744);
+}
+if (!fs.existsSync('./code/c')) {
+    fs.mkdirSync('./code/c', 0744);
+}
+if (!fs.existsSync('./code/cpp')) {
+    fs.mkdirSync('./cpp', 0744);
+}
+if (!fs.existsSync('./code/node')) {
+    fs.mkdirSync('./code/node', 0744);
+}
+if (!fs.existsSync('./code/python')) {
+    fs.mkdirSync('./code/python', 0744);
+}
+if (!fs.existsSync('./code/java')) {
+    fs.mkdirSync('./code/java', 0744);
+}
+
 
 module.exports = {
     runC: c_support.runC

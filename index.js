75e4aac751956c24954ba0dc3626a8b0266b12dd
vibhor1997a <vibhor349a@rediffmail.com>
2017-10-17 10:12:10 +0530

    Run By File

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var c_support = require('./lib/c-support.js');
 var cpp_support = require('./lib/cpp-support.js');
-var java_support=require('./lib/java-support.js');
-var python_support=require('./lib/python-support.js');
-var node_support=require('./lib/node-support.js');
-var fs=require('fs');
-
+var java_support = require('./lib/java-support.js');
+var python_support = require('./lib/python-support.js');
+var node_support = require('./lib/node-support.js');
+var fs = require('fs');
 if (!fs.existsSync('./code')) {
     fs.mkdirSync('./code', 0744);
 }
@@ -23,12 +22,12 @@ if (!fs.existsSync('./code/python')) {
 if (!fs.existsSync('./code/java')) {
     fs.mkdirSync('./code/java', 0744);
 }
-
-
 module.exports = {
     runC: c_support.runC
     , runCpp: cpp_support.runCpp
     , runJava: java_support.runJava
     , runPython: python_support.runPython
     , runNode: node_support.runNode
+    , runCFile: c_support.runCFile
+    , runCppFile:cpp_support.runCppFile
 }

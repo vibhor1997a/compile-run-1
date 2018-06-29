5a5c590b66e1332f5cac4081a527f93f6a686c2a
vibhor1997a <vibhor349a@rediffmail.com>
2018-06-29 18:27:52 +0530

    New Version Initial Commit

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-var c_support = require('./lib/c-support.js');
-var cpp_support = require('./lib/cpp-support.js');
-var java_support = require('./lib/java-support.js');
-var python_support = require('./lib/python-support.js');
-var node_support = require('./lib/node-support.js');
-var fs = require('fs');
-var parseFilePath = function (str) {
-    var fileName = str.replace(/^.*[\\\/]/, '');
-    var filePath = str.substr(0, str.indexOf(fileName));
-    var tmp = fileName.split('.');
-    var fileExt = '.' + tmp[tmp.length - 1];
+let c_support = require('./lib/c-support.js');
+let cpp_support = require('./lib/cpp-support.js');
+let java_support = require('./lib/java-support.js');
+let python_support = require('./lib/python-support.js');
+let node_support = require('./lib/node-support.js');
+let fs = require('fs');
+
+/**
+ * @typedef {Object} ParsedFilePath
+ * @property {string} fileName 
+ * @property {string} filePath 
+ * @property {string} fileExt 
+ */
+
+/**
+ * Parses the file path
+ * @param {string} str Path Like String
+ * @returns {ParsedFilePath} Parsed File Path Obj
+ */
+function parseFilePath(str) {
+    let fileName = str.replace(/^.*[\\\/]/, '');
+    let filePath = str.substr(0, str.indexOf(fileName));
+    let tmp = fileName.split('.');
+    let fileExt = '.' + tmp[tmp.length - 1];
     fileName = fileName.substr(0, fileName.indexOf(fileExt));
     return {
         fileName: fileName
@@ -16,41 +29,55 @@ var parseFilePath = function (str) {
         , fileExt: fileExt
     }
 }
-var runFile = function (path, stdin, callback) {
+
+/**
+* @callback MyCallback akakkak
+* @param {Error} err Error
+* @param {string} stderr The stderr from the program execution
+* @param {string} stdout The stdout from the program execution
+*/
+
+/**
+ * Runs the file at the given path using appropriate compiler and executes the callback
+ * @param {string} path Path like string
+ * @param {string} stdin the stdin to be sent to program
+ * @param {MyCallback} callback
+ */
+function runFile(path, stdin, callback) {
     path = parseFilePath(path);
-    var ext = path.fileExt;
+    let ext = path.fileExt;
     if (ext) {
         if (ext == '.c') {
             c_support.runCFile(path.filePath, path.fileName, stdin, function (stdout, stderr) {
-                callback(stdout, stderr, "");
+                callback(stdout, stderr, undefined);
             });
         }
         else if (ext == '.cpp') {
             cpp_support.runCppFile(path.filePath, path.fileName, stdin, function (stdout, stderr) {
-                callback(stdout, stderr, "");
+                callback(stdout, stderr, undefined);
             });
         }
         else if (ext == '.java') {
             java_support.runJavaFile(path.filePath, path.fileName, stdin, function (stdout, stderr) {
-                callback(stdout, stderr, "");
+                callback(stdout, stderr, undefined);
             });
         }
         else if (ext == '.py') {
             python_support.runPythonFile(path.filePath, path.fileName, stdin, function (stdout, stderr) {
-                callback(stdout, stderr, "");
+                callback(stdout, stderr, undefined);
             });
         }
         else if (ext == '.js') {
             node_support.runNodeFile(path.filePath, path.fileName, stdin, function (stdout, stderr) {
-                callback(stdout, stderr, "");
+                callback(stdout, stderr, undefined);
             });
         }
         else {
-            callback("", "", "This file extension is not supported");
+            callback(undefined, undefined, "This file extension is not supported");
         }
     }
     else {
-        callback("", "", "No Extension specified in file path")
+        callback(undefined, undefined, "No Extension specified in file path")
     }
 }
 module.exports = {
@@ -60,4 +87,6 @@ module.exports = {
     , runPython: python_support.runPython
     , runNode: node_support.runNode
     , runFile: runFile
+    , options: {
+    }
 }
